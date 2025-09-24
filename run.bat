2b137284caba8bd8f95efcf9d2ecb3f989d7d991
OtavioRod <otavioamorimalmeida@hotmail.com>
2025-09-24 13:19:39 -0300

    venv poetry

--- run.bat
+++ run.bat
@@ -1,3 +1,11 @@
+REM .venv\Scripts\activate.bat
+REM poetry install
+REM poetry run fastapi dev mvp_archeosys/app.py
+
+call .venv\Scripts\activate.bat
+
+REM Install dependencies using Poetry
 poetry install
 
+REM Run FastAPI app using Poetry
 poetry run fastapi dev mvp_archeosys/app.py
