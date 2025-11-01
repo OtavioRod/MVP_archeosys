3b3dc2d2456f1b9d452d6608942cc54b6df6c251
OtavioRod <otavioamorimalmeida@hotmail.com>
2025-11-01 14:40:34 -0300

    teste

--- run.bat
+++ run.bat
@@ -5,5 +5,14 @@ call .venv\Scripts\activate.bat
 REM Install dependencies using Poetry
 poetry install
 
+<<<<<<< Updated upstream
 REM Run FastAPI app using Poetry
 poetry run fastapi dev mvp_archeosys/app.py
+=======
+start http://localhost:8000/app/login.html
+
+python -m poetry install
+python -m poetry run fastapi dev mvp_archeosys/app.py
+
+pause
+>>>>>>> Stashed changes
