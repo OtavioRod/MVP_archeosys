39756280a43330440d7240f5ce9a047b7e44c655
OtavioRod <77636636+OtavioRod@users.noreply.github.com>
2026-05-22 17:40:53 -0600

    Mudança ordem de comandos run.bat

--- run.bat
+++ run.bat
@@ -5,10 +5,11 @@ REM Instala Poetry dentro do venv
 python -m pip install --upgrade pip
 python -m pip install poetry
 
-start http://localhost:8000/app/login.html
+
 
 REM Agora você pode usar
 python -m poetry install
+start http://localhost:8000/app/login.html
 python -m poetry run fastapi dev mvp_archeosys/app.py
 
 pause
