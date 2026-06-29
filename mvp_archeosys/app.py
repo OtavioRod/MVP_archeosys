30070859c5fdbc7ac2e8107905e7424b1d405880
OtavioRod <otavioamorimalmeida@hotmail.com>
2026-06-29 20:16:31 -0300

    variavel global redundante removida

--- mvp_archeosys/app.py
+++ mvp_archeosys/app.py
@@ -70,7 +70,6 @@ def prepare_base():
 
     DATABASE_URL = "postgresql://postgres:admin@localhost:5432/MVP"
     engine = create_engine(DATABASE_URL)
-    global engine, Base, SessionLocal, session, metadata
     engine = create_engine(URL_DATABASE)#env
     Base = automap_base()
     Base.prepare(autoload_with=engine)
